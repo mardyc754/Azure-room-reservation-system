9be72b63a8f5f231adae2b4a65477c2caa8cb5aa
Marta Dychała <mar754dyc@gmail.com>
2025-01-26 18:31:26 +0100

    Restore Home file (casing issues)

---
+++ app/routes/Home.tsx
@@ -0,0 +1,23 @@
+import { PageWrapper } from '@/components/PageWrapper';
+
+import type { Route } from './+types/Home';
+
+import { useAuthProvider } from '@/providers/AuthProvider';
+import { HomePageButtons } from '@/components/HomePageButtons';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'Azure Room Reservation System' },
+    { name: 'description', content: 'Welcome to Room reservation system!' }
+  ];
+}
+
+export default function Home() {
+  const { currentUser } = useAuthProvider();
+
+  return (
+    <PageWrapper title="Welcome to Room reservation system!">
+      <HomePageButtons />
+    </PageWrapper>
+  );
+}
